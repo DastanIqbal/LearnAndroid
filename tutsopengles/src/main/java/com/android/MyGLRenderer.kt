500b5eb2ae99e564d9e3acead72db5e4f7c0fe78
Iqbal Ahmed (Sinbad) <ask2iqbal@gmail.com>
2018-02-13 23:54:36 +0800

    added touch to rotate triangle

--- tutsopengles/src/main/java/com/android/MyGLRenderer.kt
+++ tutsopengles/src/main/java/com/android/MyGLRenderer.kt
@@ -3,7 +3,6 @@ package com.android
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
-import android.os.SystemClock
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -12,22 +11,23 @@ import javax.microedition.khronos.opengles.GL10
  * dastanIqbal@marvelmedia.com
  * 12/02/2018 9:50
  */
-class MyGLRenderer : GLSurfaceView.Renderer {
+class MyGLRenderer(var angle: Float) : GLSurfaceView.Renderer {
     private var mTriangle: Triangle? = null
     private val mMVPMatrix = FloatArray(16)//MVP Matrix
     private val mProjectionMatrix = FloatArray(16) //Map OpenGL coordinate systems to Device Coordinate system
     private val mViewMatrix = FloatArray(16) //CameraView
     private val mRotationMatrix = FloatArray(16) //Rotation Matrix
     override fun onDrawFrame(p0: GL10?) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
         // Set the camera position (View matrix)
         Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, -6f, 0f, 0f, 0f, 0f, 1f, 0f)
 
         // Calculate the projection and view transformation
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
 
-        // Create a rotation transformation for the triangle
-        val time = SystemClock.uptimeMillis() % 4000L
-        val angle = 0.90f * time
+//         Create a rotation transformation for the triangle
+//        val time = SystemClock.uptimeMillis() % 4000L
+//        val angle = 0.90f * time
         Matrix.setRotateM(mRotationMatrix, 0, angle, 0f, 0f, -1f)
         val scratch = FloatArray(16)
         // Combine the rotation matrix with the projection and camera view
--- tutsopengles/src/main/java/com/android/MyGLSurfaceView.kt
+++ tutsopengles/src/main/java/com/android/MyGLSurfaceView.kt
@@ -2,6 +2,7 @@ package com.android
 
 import android.content.Context
 import android.opengl.GLSurfaceView
+import android.view.MotionEvent
 
 /**
  * Created by dastaniqbal on 12/02/2018.
@@ -10,11 +11,37 @@ import android.opengl.GLSurfaceView
  */
 
 class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+    private val TOUCH_SCALE_FACTOR = 180.0f / 320
+    private var mPreviousX: Float = 0.toFloat()
+    private var mPreviousY: Float = 0.toFloat()
+    var mRenderer: MyGLRenderer
+
     init {
         setEGLContextClientVersion(2)
         setEGLConfigChooser(8, 8, 8, 8, 8, 8)
-        setRenderer(MyGLRenderer())
-       // renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+        mRenderer = MyGLRenderer(0f)
+        setRenderer(mRenderer)
+        renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+        when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                var dx = x - mPreviousX
+                var dy = y - mPreviousY
+                // reverse direction of rotation above the mid-line
+                if (y > height / 2) dx *= -1
+                // reverse direction of rotation to left of the mid-line
+                if (x > width / 2) dy *= -1
+                mRenderer.angle = mRenderer.angle + (dx + dy) * TOUCH_SCALE_FACTOR
+                requestRender()
+            }
+        }
+        mPreviousX = x
+        mPreviousY = y
+        return true
     }
 
 }
