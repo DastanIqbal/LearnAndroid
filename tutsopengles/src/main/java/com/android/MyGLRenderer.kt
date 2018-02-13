fb05a2f2961da8a79f524c5da3fe7fabccce897b
Iqbal Ahmed (Sinbad) <ask2iqbal@gmail.com>
2018-02-13 10:14:42 +0800

    wip: working on lookatm

--- tutsopengles/src/main/java/com/android/MyGLRenderer.kt
+++ tutsopengles/src/main/java/com/android/MyGLRenderer.kt
@@ -2,6 +2,7 @@ package com.android
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -12,32 +13,30 @@ import javax.microedition.khronos.opengles.GL10
  */
 class MyGLRenderer : GLSurfaceView.Renderer {
     private var mTriangle: Triangle? = null
-
+    private val mMVPMatrix = FloatArray(16)//MVP Matrix
+    private val mProjectionMatrix = FloatArray(16) //Map OpenGL coordinate systems to Device Coordinate system
+    private val mViewMatrix = FloatArray(16) //CameraView
     override fun onDrawFrame(p0: GL10?) {
-        mTriangle?.draw()
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0f, 0.5f, -5f, 0f, 0f, 0f, 0f, 0f, 0f)
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
+
+        //Draw Shape
+        mTriangle?.draw(mMVPMatrix)
     }
 
     override fun onSurfaceChanged(p0: GL10?, width: Int, height: Int) {
         GLES20.glViewport(0, 0, width, height)
+        val ratio = width.toFloat() / height.toFloat()
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 
     override fun onSurfaceCreated(p0: GL10?, p1: EGLConfig?) {
         GLES20.glClearColor(0f, 0f, 0f, 0f)
         mTriangle = Triangle()
-    }
-
-    companion object {
-        fun loadShader(type: Int, shaderCode: String): Int {
-
-            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-            val shader = GLES20.glCreateShader(type)
-
-            // add the source code to the shader and compile it
-            GLES20.glShaderSource(shader, shaderCode)
-            GLES20.glCompileShader(shader)
-
-            return shader
-        }
     }
 }
--- tutsopengles/src/main/java/com/android/Triangle.kt
+++ tutsopengles/src/main/java/com/android/Triangle.kt
@@ -18,10 +18,16 @@ class Triangle {
             0f, 0f, 1f
     )
     val colors = floatArrayOf(1f, 1f, 1f, 1f)
+    // This matrix member variable provides a hook to manipulate
+    // the coordinates of the objects that use this vertex shader
     val vs = ("" +
+            "uniform mat4 uMVPMatrix;\n" +
             "attribute vec4 vPosition;\n" +
             "void main(){\n" +
-            "      gl_Position=vPosition;\n" +
+            // the matrix must be included as a modifier of gl_Position
+            // Note that the uMVPMatrix factor *must be first* in order
+            // for the matrix multiplication product to be correct.
+            "      gl_Position=uMVPMatrix*vPosition;\n" +
             "}\n" +
             "")
     val fs = ("" +
@@ -36,6 +42,8 @@ class Triangle {
     private var mProgram: Int = -1
     private var mPositionHandler: Int = -1
     private var mColorHandle: Int = -1
+    // Use to access and set the view transformation
+    private var mMVPMatrixHandle: Int = -1
 
     init {
         val bb = ByteBuffer.allocateDirect(vertices.size * 4)
@@ -48,7 +56,7 @@ class Triangle {
         mProgram = GLUtils.loadProgram(vs, fs);
     }
 
-    fun draw() {
+    fun draw(mvpMatrix: FloatArray) {
         GLES20.glUseProgram(mProgram)
         mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition")
         GLES20.glEnableVertexAttribArray(mPositionHandler)
@@ -58,6 +66,11 @@ class Triangle {
 //
 //        // Set color for drawing the triangle
 //        GLES20.glUniform4fv(mColorHandle, 1, colors, 0)
+
+        // get handle to shape's transformation matrix
+        mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix")
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix,0)
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertices.size / 3)
         GLES20.glDisableVertexAttribArray(mPositionHandler)
