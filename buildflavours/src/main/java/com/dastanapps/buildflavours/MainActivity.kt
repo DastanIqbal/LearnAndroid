a4f4b5e637001c337cebd1028b98e1bd898cdd50
iqbal <iqbal.ahmed@xische.com>
2023-10-16 15:27:57 +0400

    find current flavour

--- buildflavours/src/main/java/com/dastanapps/buildflavours/MainActivity.kt
+++ buildflavours/src/main/java/com/dastanapps/buildflavours/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.dastanapps.buildflavours
 
+import android.graphics.Color
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.dastanapps.buildflavours.ads.AdsConfig
@@ -11,7 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val text = findViewById<TextView>(R.id.text)
+        val webview = findViewById<WebView>(R.id.webview)
 
         text.text = AdsConfig().callMe(this)
+        webview.setBackgroundColor(Color.RED)
+        webview.loadUrl("https://tamm.abudhabi")
     }
 }
