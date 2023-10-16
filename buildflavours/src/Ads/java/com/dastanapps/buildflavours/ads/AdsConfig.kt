89eed55e9461eb934aa6e3d597411ffbb01f0585
iqbal <iqbal.ahmed@xische.com>
2023-10-16 18:04:00 +0400

    write in config.properties

--- buildflavours/src/Ads/java/com/dastanapps/buildflavours/ads/AdsConfig.kt
+++ buildflavours/src/Ads/java/com/dastanapps/buildflavours/ads/AdsConfig.kt
@@ -7,9 +7,10 @@ import com.dastanapps.buildflavours.BuildConfig
 
 class AdsConfig {
 
-    fun callMe(context: Context) {
+    fun callMe(context: Context): String {
         val message = "Current Flavour ${BuildConfig.FLAVOR}"
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         Log.d("DEBUG", message)
+        return message
     }
 }
